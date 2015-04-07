cb9dbf5f00db49ec55dae7fd011d325f2d782b55
Zoom NGUYEN <frtlupsvn@gmail.com>
2015-04-07 18:42:25 +0700

    tab bar Player : 1st day

---
+++ iNhac/PlayerView.swift
@@ -0,0 +1,21 @@
+//
+//  PlayerView.swift
+//  iNhac
+//
+//  Created by Zoom NGUYEN on 4/7/15.
+//  Copyright (c) 2015 ZoomStudio. All rights reserved.
+//
+
+import UIKit
+
+class PlayerView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
